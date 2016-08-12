385789a95a621acdafa737f13da5c51d577cb396
Jarrod Koch <Jarrod Koch>
2016-08-11 20:34:39 -0500

    Update app structure to reflect intent

---
+++ app/pages/convert/convert.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ConvertPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/convert/convert.html',
+})
+export class ConvertPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
